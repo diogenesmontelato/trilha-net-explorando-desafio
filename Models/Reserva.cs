d4cbaaab482167e814fa9e4ef80d6e039fcff2a9
Diógnees Montelato <diogenes.montelato@gmail.com>
2025-08-06 21:52:43 -0300

    Desafio finlizado

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +25,9 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("A quantidade de hospedes não pode maior que capacidade da suite");
+                
+
             }
         }
 
@@ -37,7 +40,9 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            int qtdHospedes = Hospedes.Count;
+
+            return qtdHospedes;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,16 +50,18 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            //decimal valorComDesconto = 0;
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= 0.9m;
+                return valor;
             }
 
-            return valor;
+           return valor;
         }
     }
 }
